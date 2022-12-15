5dd3ed1ff984afb7b71995f07f030ec919bb741c
ficbauer <radek.ficbauer@gmail.com>
2022-12-15 10:32:24 +0100

    Add files via upload

---
+++ VisualStudioUser_Detection.ps1
@@ -0,0 +1,22 @@
+﻿#Fill this variable with the Winget package ID
+#$PackageName = "Adobe.Acrobat.Reader.64-bit"
+$PackageName = "Microsoft.VisualStudioCode"
+
+$ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
+    if ($ResolveWingetPath){
+           $WingetPath = $ResolveWingetPath[-1].Path
+    }
+
+$config
+cd $wingetpath
+
+$InstalledApps = .\winget.exe list --id $PackageName
+
+if ($InstalledApps) {
+    Write-Host "$($PackageName) is installed"
+    Exit 0
+}
+else {
+    Write-Host "$($PackageName) not detected"
+    Exit 1
+}
---
+++ VisualStudioUser_Upgrade.ps1
@@ -0,0 +1,30 @@
+﻿$PackageName = "Microsoft.VisualStudioCode"
+
+$AppInstaller = Get-AppxProvisionedPackage -Online | Where-Object DisplayName -eq Microsoft.DesktopAppInstaller
+
+#Start Logging
+Start-Transcript -Path "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\$($PackageName)_Install.log" -Append
+
+#Trying to install Package with Winget
+IF ($PackageName){
+    try {
+        Write-Host "Installing $($PackageName) via Winget" -ForegroundColor Green
+
+        $ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
+        if ($ResolveWingetPath){
+               $WingetPath = $ResolveWingetPath[-1].Path
+        }
+    
+        $config
+        cd $wingetpath
+
+        .\winget.exe upgrade --id $PackageName --silent --accept-source-agreements --accept-package-agreements
+    }
+    Catch {
+        Throw "Failed to install package $($_)"
+    }
+}
+Else {
+    Write-Host "Package $($PackageName) not available" -ForegroundColor Yellow
+}
+Stop-Transcript
